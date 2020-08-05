407a686b35c4f55b637d0b21b0bb765c67bdb071
shakespete <petersyjuco@gmail.com>
2020-08-05 22:33:13 +0800

    Reverse words refactor

--- reverse_words.cpp
+++ reverse_words.cpp
@@ -31,23 +31,26 @@ void reverseWord(string& str, int leftIdx, int rightIdx) {
     }
 }
 
-int main() {
-    string test_str = "others with yourself compare you If";;
-    printf("%s\n", test_str.c_str());
-    reverseChar(test_str);
+void reverseWords(string& str) {
+    reverseChar(str);
     
     size_t l = 0;
     size_t r = 0;
-    while (test_str[r] != '\0') {
-        if (test_str[r] == ' ') {
-            reverseWord(test_str, l, r - 1);
+    while (str[r] != '\0') {
+        if (str[r] == ' ') {
+            reverseWord(str, l, r - 1);
             l = r + 1;
         }
         ++r;
     }
-    reverseWord(test_str, l, r - 1);
+    reverseWord(str, l, r - 1);
     l = r + 1;
-    
+}
+
+int main() {
+    string test_str = "others with yourself compare you If";;
+    printf("%s\n", test_str.c_str());
+    reverseWords(test_str);
     printf("%s\n", test_str.c_str());
     return 0;
 }
