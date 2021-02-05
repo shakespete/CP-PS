28d7ee3fa9312f35ff8d6fd483d84201c40bc1db
shakespete <petersyjuco@gmail.com>
2021-02-05 15:38:22 +1100

    HR: Choco Feast - Accepted

--- HR_algo_implem_chocolate-feast/HR_algo_implem_chocolate-feast/main.cpp
+++ HR_algo_implem_chocolate-feast/HR_algo_implem_chocolate-feast/main.cpp
@@ -9,28 +9,33 @@
 #include <stdio.h>
 
 int chocolateFeast(int n, int c, int m) {
-    int leftOverCash = n % c;
-    int bars = n / c;
-    printf("Cash: %d\n", leftOverCash);
-    printf("Bars: %d\n", bars);
-    
-    int wrappers = bars;
-    printf("Wrappers: %d\n", wrappers);
-    bars -= wrappers;
-    printf("Remaining Bars: %d\n", bars);
-    
-    
-    bars += wrappers / m;
-    printf("New Bars: %d\n", bars);
-    wrappers -= bars * m;
-    printf("Remaining Wrappers: %d\n", wrappers);
+    int bars = 0;
+    int eatIdx = 0;
+    int wrappers = 0;
+    int* eaten = new int[n];
+
+    bars = n / c;
+    eaten[eatIdx++] = bars;
     wrappers += bars;
     
+    while (bars >= 0 && wrappers >= m) {
+        bars -= eaten[eatIdx - 1];
+        bars += wrappers / m;
+        eaten[eatIdx++] = bars;
+        wrappers -= bars * m;
+        wrappers += bars;
+    }
     
-    return 1;
+    int ans = 0;
+    for (int i = 0; i < eatIdx; ++i) ans += eaten[i];
+    printf("%d\n", ans);
+    return ans;
 }
 
 int main() {
     chocolateFeast(15, 3, 2);
+    chocolateFeast(10, 2, 5);
+    chocolateFeast(12, 4, 4);
+    chocolateFeast(6, 2, 2);
     return 0;
 }
