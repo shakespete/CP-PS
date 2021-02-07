e3e1753f1e5fde34ee2875350c4768461d93b74b
shakespete <petersyjuco@gmail.com>
2021-02-07 18:14:52 +1100

    HR: Queen's Attack - WIP

--- HR_algo_implem_queens-attack-2/HR_algo_implem_queens-attack-2/main.cpp
+++ HR_algo_implem_queens-attack-2/HR_algo_implem_queens-attack-2/main.cpp
@@ -13,25 +13,52 @@ using namespace std;
 vector<string> split_string(string);
 
 int GRID[100005][100005];
+int ans = 0;
+int N = 0;
+
+void traverse(int r, int c, int mRow, int mCol) {
+    int y = r + mRow;
+    int x = c + mCol;
+    if (
+        (y > 0 && y <= N) &&
+        (x > 0 && x <= N) &&
+        GRID[y][x] == 0
+       ) {
+        GRID[y][x] = 1;
+        ans++;
+        traverse(y, x, mRow, mCol);
+    }
+}
 
 int queensAttack(int n, int k, int r_q, int c_q, vector<vector<int>> obstacles) {
-    int ans = 0;
+    // #1 Reset board
+    ans = 0;
+    N = n;
     for (int i = 1; i <= n; ++i)
         for (int j = 1; j <= n; ++j)
             GRID[i][j] = 0;
     
+    // #2 Set start pos
     GRID[r_q][c_q] = 9;
     
+    // #3 Set obstacles
     for (int i = 0; i < obstacles.size(); ++i) {
         int r = obstacles[i][0];
         int c = obstacles[i][1];
         GRID[r][c] = -1;
-//        for (int j = 0; j < obstacles[i].size(); ++j) {
-//            printf("%d ", obstacles[i][j]);
-//        }
-//        printf("\n");
     }
-//    printf("\n");
+    
+    // Moves list
+    int rowMoves[] = {-1, 0, 1};
+    int colMoves[] = {-1, 0, 1};
+    
+    // #4 Start initial moves
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            traverse(r_q, c_q, rowMoves[i], colMoves[j]);
+        }
+    }
+    
     
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= n; ++j) {
@@ -39,7 +66,7 @@ int queensAttack(int n, int k, int r_q, int c_q, vector<vector<int>> obstacles) 
         }
         printf("\n");
     }
-    
+    printf("%d\n", ans);
     return 1;
 }
 
