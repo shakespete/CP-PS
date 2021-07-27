7ff1dd288bde18a3ea0f970d59672e40c38e2acf
shakespete <petersyjuco@gmail.com>
2021-07-27 16:38:53 +1000

    HR: Maximum Element - Accepted

--- HR_ds_stacks_maximum-element/HR_ds_stacks_maximum-element/main.cpp
+++ HR_ds_stacks_maximum-element/HR_ds_stacks_maximum-element/main.cpp
@@ -11,86 +11,6 @@
 
 using namespace std;
 
-struct Node {
-    int val;
-    Node* prev;
-    Node* next;
-};
-
-class DLL {
-public:
-    DLL();
-    ~DLL();
-    bool empty() const;
-    Node* front() const;
-    Node* back() const;
-    void add(int e, Node* v);
-    void addSort(int e);
-    void remove(Node* v);
-    void removeVal(int e);
-    void removeBack();
-private:
-    Node* head;
-    Node* tail;
-};
-
-DLL::DLL() {
-    head = new Node;
-    tail = new Node;
-    head->val = -1;
-    tail->val = -1;
-    head->next = tail;
-    tail->prev = head;
-}
-DLL::~DLL() { while (!empty()) removeBack(); }
-bool DLL::empty() const { return head->next == tail; }
-Node* DLL::front() const { return empty() ? NULL : head->next; }
-Node* DLL::back() const { return empty() ? NULL : tail->prev; }
-void DLL::add(int e, Node* v) {
-    Node* u = new Node;
-    u->val = e;
-    
-    u->next = v;
-    u->prev = v->prev;
-    v->prev->next = u;
-    v->prev = u;
-}
-void DLL::addSort(int e) {
-    Node* node = front();
-    if (node != NULL) {
-        while (node->val != -1) {
-            if (e > node->val) {
-                add(e, node);
-                return;
-            }
-            node = node->next;
-        }
-    }
-    add(e, tail);
-}
-void DLL::remove(Node* v) {
-    if (!empty()) {
-        Node* u = v->prev;
-        Node* w = v->next;
-        u->next = w;
-        w->prev = u;
-        delete v;
-    }
-};
-void DLL::removeVal(int e) {
-    if (empty()) return;
-    
-    Node* node = front();
-    while (node->val != -1) {
-        if (e == node->val) {
-            remove(node);
-            return;
-        }
-        node = node->next;
-    }
-}
-void DLL::removeBack() { if (!empty()) remove(tail->prev); };
-
 class Stack {
     enum { DEF_CAP = 100005 };
 public:
@@ -123,7 +43,6 @@ vector<int> getMax(vector<string> operations) {
     
     int N = (int)operations.size();
     Stack* st = new Stack(N);
-    DLL* dList = new DLL();
     
     vector<string>::iterator i = operations.begin();
     while(i != operations.end()) {
@@ -132,26 +51,25 @@ vector<int> getMax(vector<string> operations) {
             int pos = (int) i->find(" ");
             string str = i->substr(pos + 1).c_str();
             int val = stoi(str);
-            st->push(val);
-            dList->addSort(val);
+            
+            if (st->empty()) {
+                st->push(val);
+            } else {
+                int max_so_far = st->top();
+                
+                if (val > max_so_far) st->push(val);
+                else st->push(max_so_far);
+            }
         } else if (type == '2') {
             int popped = st->pop();
-//            printf("Popped: %d\n", popped);
-            dList->removeVal(popped);
+            printf("Popped: %d\n", popped);
         } else {
-            int max = dList->front()->val;
-//            printf("MAX: %d\n", max);
+            int max = st->top();
+            printf("MAX: %d\n", max);
             ans.push_back(max);
         }
         ++i;
     }
-    
-//    Node* node = dList->front();
-//    while (node->val != -1) {
-//        printf("%d ", node->val);
-//        node = node->next;
-//    }
-//    printf("\n");
     return ans;
 }
 
