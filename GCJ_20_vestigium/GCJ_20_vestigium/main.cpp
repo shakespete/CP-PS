365686eda1346fd9d54b06cd493a44c5f47f6a15
shakespete <petersyjuco@gmail.com>
2021-08-08 00:55:56 +1000

    Error: MLE

--- GCJ_20_vestigium/GCJ_20_vestigium/main.cpp
+++ GCJ_20_vestigium/GCJ_20_vestigium/main.cpp
@@ -6,15 +6,16 @@
 //
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string>
 
 
-int T, N;
-int* vestigium(int** grid) {
+int* vestigium(int** grid, int N) {
+    int k = 0;
+    int r = 0;
+    int c = 0;
     int* ans = new int[3];
-    ans[0] = 1;
-    ans[1] = 2;
-    ans[2] = 3;
+    ans[0] = k;
+    ans[1] = r;
+    ans[2] = c;
     
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
@@ -29,20 +30,54 @@ int* vestigium(int** grid) {
 int main() {
     freopen("input.txt", "r", stdin);
     
+    int T, N, k, r, c;
     scanf("%d", &T);
     for (int test_case = 1; test_case <= T; ++test_case) {
         scanf("%d", &N);
+        k = r = c = 0;
         
-        int** grid = new int*[N];
+        int* rowVis = new int[N]{0};
+        int** rows = new int*[N];
+        
+        int* colVis = new int[N]{0};
+        int** cols = new int*[N];
+        
         for (int i = 0; i < N; ++i) {
-            grid[i] = new int[N];
+            rows[i] = new int[N]{0};
+            cols[i] = new int[N]{0};
+        }
+        
+        for (int i = 0; i < N; ++i) {
             for (int j = 0; j < N; ++j) {
-                scanf("%d", &grid[i][j]);
+                int val;
+                scanf("%d", &val);
+                
+                if (rowVis[i] == 0 && rows[i][val - 1] == 1) {
+                    rowVis[i] = 1;
+                    r++;
+                }
+                
+                if (colVis[j] == 0 && cols[j][val - 1] == 1) {
+                    colVis[j] = 1;
+                    c++;
+                }
+                
+                rows[i][val - 1] = 1;
+                cols[j][val - 1] = 1;
+                
+                if (i == j) k += val;
             }
         }
         
-        int* ans = vestigium(grid);
-        printf("Case #%d: %d %d %d\n", test_case, ans[0], ans[1], ans[2]);
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                printf("%d ", cols[i][j]);
+            }
+            printf("\n");
+        }
+        
+//        int* ans = vestigium(grid, N);
+        printf("Case #%d: %d %d %d\n", test_case, k, r, c);
     }
     return 0;
 }
