34fcff1f2741d8f1d2b573d16261801e51b9e723
shakespete <petersyjuco@gmail.com>
2021-08-17 22:43:06 +1000

    Passed

--- GCJ_20_vestigium/GCJ_20_vestigium/main.cpp
+++ GCJ_20_vestigium/GCJ_20_vestigium/main.cpp
@@ -6,50 +6,29 @@
 //
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <vector>
+
 #include <time.h>
 
-
-int* vestigium(int** grid, int N) {
-    int k = 0;
-    int r = 0;
-    int c = 0;
-    int* ans = new int[3];
-    ans[0] = k;
-    ans[1] = r;
-    ans[2] = c;
-    
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            printf("%d ", grid[i][j]);
-        }
-        printf("\n");
-    }
-    
-    return ans;
-}
+using namespace std;
 
 int main() {
     clock_t start, end;
     start = clock();
-    
     freopen("input.txt", "r", stdin);
     
     int T, N, k, r, c;
     scanf("%d", &T);
+    
     for (int test_case = 1; test_case <= T; ++test_case) {
         scanf("%d", &N);
         k = r = c = 0;
         
-        int* rowVis = new int[N]{0};
-        int** rows = new int*[N];
+        vector<int> rowVis(N, 0);
+        vector<vector<int>> rows(N, vector<int>(N, 0));
         
-        int* colVis = new int[N]{0};
-        int** cols = new int*[N];
-        
-        for (int i = 0; i < N; ++i) {
-            rows[i] = new int[N]{0};
-            cols[i] = new int[N]{0};
-        }
+        vector<int> colVis(N, 0);
+        vector<vector<int>> cols(N, vector<int>(N, 0));
         
         for (int i = 0; i < N; ++i) {
             for (int j = 0; j < N; ++j) {
@@ -83,8 +62,8 @@ int main() {
     }
     
     end = clock();
-    
     double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
     printf("Time taken: %.6fs\n", time_taken);
+    
     return 0;
 }
