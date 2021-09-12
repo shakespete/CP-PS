ef547e6a8efa7bf6f970b25356dd0ea5ea177562
shakespete <petersyjuco@gmail.com>
2021-09-12 19:37:23 +1000

    Optimises to use of boolean vector

--- HR_algo_strings_weighted-uniform-string/HR_algo_strings_weighted-uniform-string/main.cpp
+++ HR_algo_strings_weighted-uniform-string/HR_algo_strings_weighted-uniform-string/main.cpp
@@ -10,102 +10,40 @@
 
 using namespace std;
 
-struct Node {
-    int val;
-    Node* next;
-};
-class LL {
-public:
-    LL();
-    bool empty() const;
-    Node* front() const;
-    void add(int e);
-private:
-    Node* head;
-};
-LL::LL() : head(NULL) {};
-bool LL::empty() const { return head == NULL; }
-Node* LL::front() const { return empty() ? NULL : head; }
-void LL::add(int e) {
-    Node* v = new Node;
-    v->val = e;
-    v->next = head;
-    head = v;
-}
-
-class HashMap {
-    enum { DEF_CAP = 1000 };
-public:
-    HashMap(int cap = DEF_CAP);
-    int hash(int e);
-    void insert(int e);
-    string retrieve(int e);
-private:
-    LL* HM;
-    int capacity;
-};
-
-HashMap::HashMap(int cap) : HM(new LL[cap]), capacity(cap) { }
-int HashMap::hash(int e) {
-    int hash = e * 31;
-    int hashVal = hash % capacity;
-    return hashVal;
-}
-void HashMap::insert(int e) {
-    int hashVal = hash(e);
-    Node* v = new Node;
-    v->val = e;
-    HM[hashVal].add(e);
-}
-string HashMap::retrieve(int e) {
-    int hashVal = hash(e);
-    if (HM[hashVal].empty()) return "NO";
-    
-    Node* node = HM[hashVal].front();
-    while (node) {
-        if (node->val == e) return "YES";
-        node = node->next;
-    }
-    return "NO";
-}
-
 int getLetterVal(char e) {
     return e - 'a' + 1;
 }
 
-
 vector<string> weightedUniformStrings(string s, vector<int> queries) {
     vector<string> res;
-    
-    HashMap* hm = new HashMap();
+    vector<bool> hm(10000000, false);
     
     int acc = 0;
     for (int i = 0; i < s.size(); ++i) {
         int curr = getLetterVal(s[i]);
         if (i == 0) {
-            printf("%d\n", curr);
-            hm->insert(curr);
+            printf("%c: %d\n", s[i], curr);
+            hm[curr] = true;
         } else if (getLetterVal(s[i - 1]) == curr) {
             acc += curr;
-            printf("%d\n", acc);
-            hm->insert(acc);
+            printf("%c: %d\n", s[i], acc);
+            hm[acc] = true;
         } else {
             acc = curr;
-            printf("%d\n", curr);
-            hm->insert(curr);
+            printf("%c: %d\n", s[i], curr);
+            hm[curr] = true;
         }
     }
     
     for (int i = 0; i < queries.size(); ++i) {
-        string ans = hm->retrieve(queries[i]);
-        res.push_back(ans);
+        if (hm[queries[i]]) res.push_back("YES");
+        else res.push_back("NO");
     }
-    
     return res;
 }
 
 int main(int argc, const char * argv[]) {
-    string s = "abccddde";
+    string s = "abccdddexxxyz";
     vector<int> queries = {1, 7, 5, 4, 15};
         
     vector<string> ans = weightedUniformStrings(s, queries);
