95a136817027bcc1ee22ea8a2cc2be6e8b2c52f7
shakespete <petersyjuco@gmail.com>
2021-10-12 20:43:29 +1100

    HR: Maximum Perimeter Triangle - Accepted

---
+++ HR_algo_greedy_maximum-perimeter-triangle/HR_algo_greedy_maximum-perimeter-triangle/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HR_algo_greedy_maximum-perimeter-triangle
+//
+//  Created by Jose Mari Syjuco on 12/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
