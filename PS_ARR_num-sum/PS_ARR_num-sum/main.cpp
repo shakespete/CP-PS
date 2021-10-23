fb3ca6f1c51b68446c46ddaea3baa08e3e16e79a
shakespete <petersyjuco@gmail.com>
2021-10-23 13:05:30 +1100

    Accepted

--- PS_ARR_num-sum/PS_ARR_num-sum/main.cpp
+++ PS_ARR_num-sum/PS_ARR_num-sum/main.cpp
@@ -5,10 +5,46 @@
 //  Created by Jose Mari Syjuco on 23/10/21.
 //
 
-#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+vector<vector<int>> numSum(vector<int> array, int targetSum) {
+    vector<vector<int>> result = {};
+    sort(array.begin(), array.end());
+    
+    int len = (int) array.size();
+    printf("%d\n", len);
+    for (int i = 0; i < len - 2; ++i) {
+        int a = array[i];
+        int bCursor = i + 1;
+        int cCursor = len - 1;
+        while (bCursor < cCursor) {
+            printf("CURSOR: %d %d\n", bCursor, cCursor);
+            int b = array[bCursor];
+            int c = array[cCursor];
+            int computedSum = a + b + c;
+            if (computedSum == targetSum) {
+                printf("%d %d %d\n", a, b, c);
+                result.push_back({a, b, c});
+                ++bCursor;
+                --cCursor;
+            } else {
+                if (computedSum < targetSum) ++bCursor;
+                else --cCursor;
+            }
+        }
+    }
+    return result;
+}
+
+int main() {
+    vector<int> test_1 = { -8, -6, 1, 2, 3, 5, 6, 12 };
+    vector<vector<int>> res_1 = numSum(test_1, 0);
+    for (int i = 0; i < res_1.size(); ++i) {
+        printf("%d %d %d\n", res_1[i][0], res_1[i][1], res_1[i][2]);
+    }
     return 0;
 }
